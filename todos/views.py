5a5e172936576469f87c74891d4d74bd2221fad7
Mukona-Munyai <mukona.munyai@comsol.co.za>
2025-07-11 09:56:36 +0200

    first commit

---
+++ todos/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    return render(request, 'todos/task_list.html', {'tasks': tasks, 'form': form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todos/task_update.html', {'form': form, 'task': task})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'todos/task_delete.html', {'task': task})
